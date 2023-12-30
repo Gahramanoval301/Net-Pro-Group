0216e7c843ab0af7631290b3f4e22a62ab393104
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-30 20:23:06 +0300

    add pretty breadcumb with links

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,6 +1,11 @@
 import { Box, Container, Typography } from "@mui/material"
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import WhatshotIcon from '@mui/icons-material/Whatshot';
+import GrainIcon from '@mui/icons-material/Grain';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import { Link } from "react-router-dom";
 
-const PageBanner = ({ title }) => {
+const PageBanner = ({ title, titleHref='', slug, slugTitle='' }) => {
     return (
         <Box bgcolor="primary.main" py={4}
             sx={{
@@ -10,11 +15,37 @@ const PageBanner = ({ title }) => {
             }}
         >
             <Container sx={{
-                textAlign: 'center',
+                display:'flex',
+                flexDirection:'column',
+                justifyContent:'center',
+                alignItems:'center',
                 color: 'white'
             }}>
                 <Typography fontWeight={600} mb={3} variant="h4">{title}</Typography>
-                <Typography>Əsas səhifə ▶ {title}</Typography>
+                <Breadcrumbs aria-label="breadcrumb" separator=''>
+                    <Link
+                        style={{ display: 'flex', alignItems: 'center', color:'white', textDecoration:'none' }}
+                        to='/'
+                    >
+                        Əsas səhifə
+                    </Link>
+                    <Link
+                        style={{ display: 'flex', alignItems: 'center', color:'white', textDecoration:'none' }}
+                        to={`/${titleHref}`}
+                    >
+                        <PlayArrowIcon sx={{ mr: 2 , color:'white'}} />
+                        {title}
+                    </Link>
+                    {
+                        slug ? <Link
+                            style={{ display: 'flex', alignItems: 'center', color:'white', textDecoration:'none' }}
+                            to={`/${titleHref}/${slug}`}
+                        >
+                            <PlayArrowIcon sx={{ mr: 2 }} />
+                            {slugTitle}
+                        </Link> : null
+                    }
+                </Breadcrumbs>
             </Container>
         </Box>
     )
