b0b0f35e3a1c7f020023781b52986ed0207295c3
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-30 20:26:07 +0300

    props destructing

--- src/components/PageContainer/index.jsx
+++ src/components/PageContainer/index.jsx
@@ -2,11 +2,11 @@ import PageBanner from "../Banner"
 import Footer from "../Footer"
 import { Header } from "../Header"
 
-const PageContainer = ({ children, bannerTitle = "" }) => {
+const PageContainer = ({ children, titleHref='', bannerTitle = "", slug = '', slugTitle=''}) => {
     return (
         <>
             <Header/>
-            {bannerTitle && <PageBanner title={bannerTitle} />}
+            {bannerTitle && <PageBanner title={bannerTitle} slug={slug} titleHref={titleHref} slugTitle={slugTitle}/>}
             {children}
             <Footer />
         </>
