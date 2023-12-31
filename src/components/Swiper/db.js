85b42f1b973367b887470c8c90eaa425fe13de65
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-31 23:54:23 +0300

    deleting of unused word-public

--- src/components/Swiper/db.js
+++ src/components/Swiper/db.js
@@ -2,26 +2,26 @@ export const home1stsections = [
     {
         id: 0,
         title: 'Avadanlıqların Quraşdırılması və Konfiqurasiyası',
-        image: 'public/Home.png'
-    }, 
+        image: '/Home.png'
+    },
     {
         id: 1,
         title: 'Text of the printing and typesetting',
-        image: 'public/homephoto1.jpg'
-    }, 
+        image: '/homephoto1.jpg'
+    },
     {
         id: 2,
         title: 'Lorem Ipsum is not simply random text.',
-        image: 'public/homephoto2.jpg'
-    }, 
+        image: '/homephoto2.jpg'
+    },
     {
         id: 3,
         title: 'Passages of Lorem Ipsum available',
-        image: 'public/homephoto3.jpg'
-    }, 
+        image: '/homephoto3.jpg'
+    },
     {
         id: 4,
         title: 'The standard chunk of Lorem Ipsum',
-        image: 'public/homephoto4.jpg'
+        image: '/homephoto4.jpg'
     }
 ]
